6d6ffdd2248adbafc813a11781526fcc852511ba
BrolAWE <aleksejdelov@gmail.com>
2021-01-01 21:42:03 +0300

    All work

--- superlists/urls.py
+++ superlists/urls.py
@@ -20,6 +20,7 @@ from lists import views
 
 urlpatterns = [
     path('', views.home_page, name='home'),
-    path('lists/one_list/', views.view_list, name='view_list'),
+    path('lists/<str:list_id>/', views.view_list, name='view_list'),
     path('lists/new', views.new_list, name='new_list'),
+    path('lists/<str:list_id>/add_item', views.add_item, name='add_item'),
 ]
