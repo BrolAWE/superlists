fb59fcdaab14e1863c7c9c854604b3c0b065c05e
BrolAWE <aleksejdelov@gmail.com>
2021-01-02 02:13:55 +0300

    Рефакторизация POST

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -44,22 +44,32 @@ class ListViewTest(TestCase):
         response = self.client.get(f'/lists/{correct_list.id}/')
         self.assertEqual(response.context['list'], correct_list)
 
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        '''тест: можно сохранить post-запрос в существующий список'''
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        self.client.post(
+            f'/lists/{correct_list.id}/',
+            data={'item_text': 'A new item for an existing list'}
+        )
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new item for an existing list')
+        self.assertEqual(new_item.list, correct_list)
+
+    def test_POST_redirects_to_list_view(self):
+        '''тест: post-запрос переадресуется в представление списка'''
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        response = self.client.post(
+            f'/lists/{correct_list.id}/',
+            data={'item_text': 'A new item for an existing list'}
+        )
+        self.assertRedirects(response, f'/lists/{correct_list.id}/')
+
 
 class NewListTest(TestCase):
     '''тест нового списка'''
-
-    def test_can_save_a_POST_request(self):
-        '''тест: можно сохранить post-запрос'''
-        self.client.post('/lists/new', data={'item_text': 'A new list item'})
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new list item')
-
-    def test_redirects_after_POST(self):
-        '''тест: переадресуется после post-запроса'''
-        response = self.client.post('/lists/new', data={'item_text': 'A newlist item'})
-        new_list = List.objects.first()
-        self.assertRedirects(response, f'/lists/{new_list.id}/')
 
     def test_validation_errors_are_sent_back_to_home_page_template(self):
         '''тест: ошибки валидации отсылаются назад в шаблон
