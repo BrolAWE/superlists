b5dd454f38e60d4b03d879a5f6a3eb4ae73bd3f8
BrolAWE <aleksejdelov@gmail.com>
2021-01-01 17:21:02 +0300

    Update FT

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 import unittest
+from selenium.webdriver.common.keys import Keys
+import time
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -7,7 +9,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         '''установка'''
-        self.browser = webdriver.Firefox()
+        self.browser = webdriver.Chrome("C:/Users/aleks/Downloads/chromedriver_win32/chromedriver.exe")
 
     def tearDown(self):
         '''демонтаж'''
@@ -16,16 +18,36 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         '''тест: можно начать список и получить его позже'''
-
-        # Эдит слышала про крутое новое онлайн-приложение со
-        # списком неотложных дел. Она решает оценить его
-        # домашнюю страницу
+        # Эдит слышала про крутое новое онлайн-приложение со списком
+        # неотложных дел. Она решает оценить его домашнюю страницу
         self.browser.get('http://localhost:8000')
-        # Она видит, что заголовок и шапка страницы говорят о
-        # списках неотложных дел
+        # Она видит, что заголовок и шапка страницы говорят о списках
+        # неотложных дел
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Закончить тест!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
         # Ей сразу же предлагается ввести элемент списка
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+        inputbox.send_keys('Купить павлиньи перья')
+        # Когда она нажимает enter, страница обновляется, и теперь страница
+        # содержит "1: Купить павлиньи перья" в качестве элемента списка
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Купить павлиньи перья' for row in rows)
+        )
+        # Текствое поле по-прежнему приглашает ее добавить еще один элемент.
+        # Она вводит "Сделать мушку из павлиньих перьев"
+        # (Эдит очень методична)
+        self.fail('Закончить тест!')
+        # Страница снова обновляется и теперь показывает оба элемента
+        # ее списка
 
 
 if __name__ == '__main__':
