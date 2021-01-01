0fe64a95ff0fba1f2a587e770e50663f5b8cb57d
BrolAWE <aleksejdelov@gmail.com>
2021-01-02 02:36:16 +0300

    Правило DRY

--- lists/views.py
+++ lists/views.py
@@ -10,15 +10,6 @@ def home_page(request):
     return render(request, 'home.html')
 
 
-def view_list(request, list_id):
-    '''представление списка'''
-    list_ = List.objects.get(id=list_id)
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'], list=list_)
-        return redirect(f'/lists/{list_.id}/')
-    return render(request, 'list.html', {'list': list_})
-
-
 def new_list(request):
     '''новый список'''
 
@@ -31,7 +22,7 @@ def new_list(request):
         list_.delete()
         error = "You can't have an empty list item"
         return render(request, 'home.html', {"error": error})
-    return redirect(f'/lists/{list_.id}/')
+    return redirect(list_)
 
 
 def view_list(request, list_id):
@@ -43,7 +34,7 @@ def view_list(request, list_id):
             item = Item(text=request.POST['item_text'], list=list_)
             item.full_clean()
             item.save()
-            return redirect(f'/lists/{list_.id}/')
+            return redirect(list_)
         except ValidationError:
             error = "You can't have an empty list item"
     return render(request, 'list.html', {'list': list_, 'error': error})
