0fb3f63ef5784c09957af9e82a473b77d3451a82
BrolAWE <aleksejdelov@gmail.com>
2021-01-02 15:11:50 +0300

    Финальная фиксация

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,5 +1,6 @@
-var initialize = function () {
-    $('input[name="text"]').on('keypress', function () {
-        $('.has-error').hide();
-    });
+window.Superlists = {}; 1
+window.Superlists.initialize = function () { 2
+ $('input[name="text"]').on('keypress', function () {
+     $('.has-error').hide();
+ });
 };
