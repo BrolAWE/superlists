b6b321dd821a98de02fdb7da136ff9eb59ec7b9e
BrolAWE <aleksejdelov@gmail.com>
2021-01-03 17:36:13 +0300

    Конец

--- superlists/settings.py
+++ superlists/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'zgmm71+%x846e$f)xxzeb$@+vqfk-=lfo=37k&codk=wbibdg!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
